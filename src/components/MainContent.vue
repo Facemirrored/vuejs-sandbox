199d258fbcbb72d1af2441fa368a9b8b88f6911f
Bjarne Herrmann <herrmann.bjarne@outlook.de>
2020-05-01 18:56:41 +0200

    First Component exercise

---
+++ src/components/MainContent.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="row">
+    <server-list-component></server-list-component>
+    <server-details-component></server-details-component>
+  </div>
+</template>
+
+<script>
+  import ServerList from './ServerList'
+  import ServerDetails from "./ServerDetails";
+
+  export default {
+    components: {
+      serverListComponent: ServerList,
+      serverDetailsComponent: ServerDetails
+    }
+  }
+</script>
